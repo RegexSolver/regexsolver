afbacc6941d046d444aeb0e75d3092bba6fe037d
Alexandre van Beurden <1949482+alexvbrdn@users.noreply.github.com>
2025-09-16 21:24:28 +0200

    add test

--- src/fast_automaton/convert/to_regex/transform/mod.rs
+++ src/fast_automaton/convert/to_regex/transform/mod.rs
@@ -14,3 +14,34 @@ pub fn transform(automaton: &FastAutomaton) -> FastAutomaton {
 
     automaton
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        fast_automaton::convert::to_regex::transform::transform, regex::RegularExpression,
+    };
+
+    #[test]
+    fn test_equivalence() -> Result<(), String> {
+        assert_equivalent("abc");
+        assert_equivalent(".*abc");
+        assert_equivalent(".*abc.*def");
+        assert_equivalent(".*abc.*def(ab|fr)");
+        assert_equivalent(".*abc.*def(ab|fr).*mpa");
+
+        Ok(())
+    }
+
+    fn assert_equivalent(pattern: &str) {
+        let before = RegularExpression::parse(pattern, false)
+            .unwrap()
+            .to_automaton()
+            .unwrap();
+
+        let before = before.determinize().unwrap();
+
+        let after = transform(&before);
+
+        assert!(before.equivalent(&after).unwrap());
+    }
+}
