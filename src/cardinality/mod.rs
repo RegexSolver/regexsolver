02852f967f334c40979b7328a27fbdaffb3beaec
Alexandre van Beurden <1949482+alexvbrdn@users.noreply.github.com>
2025-09-24 21:57:28 +0200

    fix serialization

--- src/cardinality/mod.rs
+++ src/cardinality/mod.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 /// Represent a number.
 #[cfg_attr(feature = "serializable", derive(Serialize, Deserialize))]
 #[derive(PartialEq, Eq, Debug, Clone)]
-#[cfg_attr(feature = "serializable", serde(tag = "type", content = "value"))]
+#[cfg_attr(feature = "serializable", serde(tag = "type", content = "value", rename_all = "camelCase"))]
 pub enum Cardinality<U> {
     /// An infinite number.
     Infinite,
